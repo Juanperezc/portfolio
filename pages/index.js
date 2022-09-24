f03c32fe04a699bffbff03237471b916e1882329
Juan Perez <juanl1996@hotmail.com>
2022-09-24 01:43:34 -0300

    install tailwind and globalcss

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <span className="text-blue-600">Test</span>
+      </main>
+    </div>
+  );
+}
